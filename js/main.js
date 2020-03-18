9f8c5edee50fbc197a88fa114789eb2250a10ca1
guohaomeng <3020721699@qq.com>
2020-03-18 18:06:32 +0800

    修bug

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ $(document).ready(
     function () {
         login();
         console.log('ok');
-       
+
     }
 );
-function checkMsg(){
-    var checkmsg = setInterval(function(){
-        if($("#msg").html()!= ""){
-            setTimeout(function(){
+function checkMsg() {
+    var checkmsg = setInterval(function () {
+        if ($("#msg").html() != "") {
+            setTimeout(function () {
                 $("#msg").html("");
-            },3000);
+            }, 3000);
         }
-    },100);
+    }, 100);
 }
 function login() {
     $("#login").css("display", "block");
@@ -26,6 +26,9 @@ function register() {
     $("#login").css("display", "none");
     $('#btn2').css('background', '#fff');
     $('#btn1').css('background', '#EBEEF5');
+    $("#msg").html("");
+}
+function reset1() {
     $("#msg").html("");
 }
 function checkLogin() {
@@ -43,14 +46,14 @@ function checkRegister() {
     //var passwd0 = $('#passwd0').val();
     //var usr1 = $("#usr1").val();
     var email = $("#email").val();
-    if((checkPasswd())&&(checkPhone())&&(checkEmail())){
+    if ((checkPasswd()) && (checkPhone()) && (checkEmail())) {
         $("#msg").html("注册成功！");
         $(".btns1 button")[1].click();
         $(".btns1 button")[2].click();
     }
-    else{
-        $("#msg").html("请检查所填信息格式是否有误！");
-        $("#usr1").focus();
+    else {
+        //$("#msg").html("请检查所填信息格式是否有误！");
+        //$("#usr1").focus();
     }
 }
 function checkPhone() {
@@ -70,40 +73,42 @@ function checkPhone() {
         return true;
     }
 }
-function checkPasswd(){
+function checkPasswd() {
     var passwd0 = $('#passwd0').val();
     var passwd1 = $('#passwd1').val();
-    if(passwd0 == ""){
+    if (passwd0 == "") {
         $("#msg").html("密码不能为空");
         $("#passwd0").focus();
         return false;
     }
-    else{
-        if(passwd0 != passwd1){
+    else {
+        if (passwd0 != passwd1) {
             $("#msg").html("两次输入密码不一致！");
             $("#passwd0").focus();
             return false;
         }
-        else{
+        else {
             return true;
         }
     }
-    
+
 }
-function checkEmail(){
+function checkEmail() {
     //定义正则表达式的变量:邮箱正则
-    var reg=/^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    var reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
     var email = $("#email").val();
-    if(email == ''){
-        $("#msg").html('email不能为空');
+    if (email == '') {
+        $("#msg").html('email不能为空！');
+        $("#email").focus();
         return false;
     }
-    else{
-        if(email.search(reg) != -1){
+    else {
+        if (email.search(reg) != -1) {
             return true;
         }
-        else{
-            $("#msg").html("邮箱格式有误");
+        else {
+            $("#msg").html("邮箱格式有误！");
+            $("#email").focus();
             return false;
         }
     }
